4e9158f30faaca3601a44bf4fbdff6f7d0a73484
Oguzhan Meric <meric.oguzhan@gmail.com>
2023-04-19 00:51:17 +0300

    gorev tamamlandi

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
+import { getByPlaceholderText } from '@testing-library/dom';
 import './less/index.less'
 
-// Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
-
-
 // Kodlar buraya gelecek!
+
+document.querySelector("h1").addEventListener("click",function(e){
+alert("Bana tıkladın!")
+});
+
+window.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    alert("Sağ tık kapatıldı");
+})
+
+
+document.querySelectorAll("h2").forEach(headerTwo=> {
+    headerTwo.addEventListener("mouseenter",e => {
+        headerTwo.style.color="#155263";
+        headerTwo.style.backgroundColor="#ff6f3c"});
+
+    headerTwo.addEventListener("mouseleave",e => {
+        headerTwo.style.color="black"
+        headerTwo.style.backgroundColor="";
+        });
+    });
+
+document.querySelectorAll("img").forEach(image =>{
+    image.addEventListener("mouseover",(e) => {
+        image.style.scale="1.3";
+
+    image.addEventListener("mouseout",(e) => {
+           image.style.scale="";
+
+    });
+    });
+});
+
+
+
+
